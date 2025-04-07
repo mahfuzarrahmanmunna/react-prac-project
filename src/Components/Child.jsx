8e19d81332d917c3414beebc3460b3e69d829ccb
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-08 02:04:33 +0600

    ajke ghumabo

---
+++ src/Components/Child.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Child = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nulla dignissimos asperiores eius quo iusto est voluptas perferendis maiores assumenda saepe, quidem dolores id similique modi quam possimus? Magnam, inventore!</p>
+        </div>
+    );
+};
+
+export default Child;
